4b4f40c1ede01c47e529764fddb7ff1a80ca6982
Sabrina Hanson <sabhanson7@gmail.com>
2022-06-26 14:29:29 -0700

    added Setup functionality, variables, map sizing

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 using namespace std;
 bool gameOver;
+const int width = 20;
+const int height = 20; // map dimensions
+int x,y, fruitX, fruitY, score;
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN};
+eDirection dir;
 void Setup()
 {
     gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width; // sets the fruits to random coordinates on the map
+    fruitY = rand() % height;
+    score = 0;
 }
 void Draw()
 {
@@ -20,5 +31,12 @@ void Logic()
 int main()
 {
     Setup();
+    while (!gameOver)
+    {
+        Draw();
+        Input();
+        Logic();
+    }
     return 0;
+
 }
