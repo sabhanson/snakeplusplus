a0735e19f067aa3c74a1f382c8f8b9ab3eca3588
Sabrina Hanson <sabhanson7@gmail.com>
2022-06-26 14:23:00 -0700

    initial setup

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+bool gameOver;
+void Setup()
+{
+    gameOver = false;
+}
+void Draw()
+{
+
+}
+void Input()
+{
+
+}
+void Logic()
+{
+
+}
+int main()
+{
+    Setup();
+    return 0;
+}
